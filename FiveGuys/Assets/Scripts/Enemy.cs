04bbe5d2fbbdb0926cd72ae6a4bad7fe6c95ed7d
IstvanW05 <163757232+IstvanW05@users.noreply.github.com>
2025-03-09 18:30:54 -0400

    Scripts

---
+++ FiveGuys/Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    private float countdown = 5f;
+
+    private Spawner waveSpawner;
+
+    private void Start()
+    {
+        waveSpawner = GetComponentInParent<Spawner>();
+    }
+    private void Update()
+    {
+
+        countdown -= Time.deltaTime;
+
+        if (countdown <= 0)
+        {
+            Destroy(gameObject);
+
+            waveSpawner.waves[waveSpawner.currentWaveIndex].enemiesLeft--;
+        }
+    }
+}
