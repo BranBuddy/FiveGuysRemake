5327138d4c2921a7172b9767c730a621da6d5b7c
BranBuddy <brandonwahl111@gmail.com>
2025-03-11 14:09:37 -0400

    added enemy types

--- FiveGuys/Assets/Scripts/EnemyFollow.cs
+++ FiveGuys/Assets/Scripts/EnemyFollow.cs
@@ -9,29 +9,67 @@ public class EnemyFollow : MonoBehaviour
     public Transform player;
     public EnemyHealthBar healthBar;
     public float enemyLives;
-    public float maxEnemyLives = 3f;
+    public float maxEnemyLives;
+    public int enemyType;
+    public float speed;
+
 
     private NavMeshAgent enemy;
     // Start is called before the first frame update
     void Start()
     {
+        speed = 5f;
         enemy = GetComponent<NavMeshAgent>();
+        enemy.speed = speed;
         enemyLives = maxEnemyLives;
         healthBar.SetMaxHealth(maxEnemyLives);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        enemy.destination = player.position;
+        enemy.SetDestination(player.position);
+        WhatEnemyTypeAmI();
+
 
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "Player" && this.gameObject.tag == "Enemy")
         {
             GameObject.Find("Player").GetComponent<Player>().Damage(1);
+        }
+        else if (other.tag == "Player" && this.gameObject.tag == "Tank")
+        {
+            GameObject.Find("Player").GetComponent<Player>().Damage(2);
+        }
+        else if (other.tag == "Player" && this.gameObject.tag == "Rusher")
+        {
+            GameObject.Find("Player").GetComponent<Player>().Damage(.5f);
+        }
+    }
+
+    public void WhatEnemyTypeAmI()
+    {
+        //base enemy
+        if(enemyType == 0)
+        {
+            
+        }
+        //tank enemy
+        else if (enemyType == 1)
+        {
+            enemy.speed = 3;
+            maxEnemyLives = 5;
+        }
+        //rushdown enemy
+        else if (enemyType == 2)
+        {
+            maxEnemyLives = 2;
+            enemy.speed = 10 - (enemyLives * 2);
+            
         }
     }
 
