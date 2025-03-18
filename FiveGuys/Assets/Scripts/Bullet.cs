744a14c281dba9df494323405105586bad2bacb5
BranBuddy <brandonwahl111@gmail.com>
2025-03-17 21:34:31 -0400

    fixed?

--- FiveGuys/Assets/Scripts/Bullet.cs
+++ FiveGuys/Assets/Scripts/Bullet.cs
@@ -7,61 +7,22 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public string enemyTag = "Enemy";
-    public float bulletSpeed = 10f;
-    private float minDistance = Mathf.Infinity;
-    private Transform targetEnemy = null;
-    private Vector3 mouseTarget;
-    public Player player;
-    private Camera cam;
-
+    private GameObject targetEnemy;
+   
+    
     void Start()
-    {
-        cam = Camera.main;
-
-        if (player.autoFire == true)
-        { // creates list of enemies
-            GameObject[] targetEnemies = GameObject.FindGameObjectsWithTag(enemyTag);
-            if (targetEnemies.Length > 0)
-            {
-                foreach (GameObject enemy in targetEnemies)
-                { // gets distance to enemies
-                    float distance = Vector3.Distance(transform.position, enemy.transform.position);
-
-                    if (distance < minDistance)
-                    { // changes target based on minimum distance found
-                        targetEnemy = enemy.transform;
-                        minDistance = distance;
-                    }
-                }
-            }
-        }
-        else
-        { // gets mouse position in screen as
-            Vector2 mousePos = new Vector2();
-            Event currentEvent = Event.current;
-
-            mousePos.x = currentEvent.mousePosition.x;
-            mousePos.y = cam.pixelHeight - currentEvent.mousePosition.y;
-
-            mouseTarget = cam.ScreenToWorldPoint(new Vector3(mousePos.x, 1, mousePos.y));
-        }
-        Invoke("DeleteSelf", 5f);
+    {    
+        // finds enemy
+        targetEnemy = GameObject.FindWithTag(enemyTag);
+        Invoke("DeleteSelf", 3f);
     }
 
     void Update()
-    {
-        // deletes bullet if no target
-        if (targetEnemy == null) DeleteSelf();
-        
-        if (player.autoFire == true)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, targetEnemy.position, bulletSpeed * Time.deltaTime);
-        }
-        else
-        {
-            transform.position = Vector3.MoveTowards(transform.position, mouseTarget, bulletSpeed * Time.deltaTime);
-        }
+    { 
 
+        // deletes bullet if no target, otherwise moves towards target
+        if (targetEnemy == null) DeleteSelf();
+        else transform.position = Vector3.MoveTowards(transform.position, targetEnemy.transform.position, 5f * Time.deltaTime);
     }
 
     void DeleteSelf()
@@ -72,8 +33,9 @@ public class Bullet : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == enemyTag)
-        { // damage enemy and destroy bullet
+        { // destroy both enemy and bullet
             GameObject.FindWithTag("Enemy").GetComponent<EnemyFollow>().enemyDamaged(1);
+
             DeleteSelf();
             
         }
--- FiveGuys/Assets/Scripts/Player.cs
+++ FiveGuys/Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine.SceneManagement;
+using System.Security.Cryptography;
 
 public class Player : MonoBehaviour
 {
@@ -14,18 +15,30 @@ public class Player : MonoBehaviour
     public float lives = 3f;
     public float maxLives = 3f;
     public float minXP = 0f;
+    public float maxSprint = 1f;
+    public float sprint;
     public float xp;
     public bool autoFire = false;
     public GameObject enemy;
     public GameObject bullet;
     private int charLevel;
+    public float sprintCost;
 
     public Healthbar healthBar;
     public XPBar xpBar;
+    public SprintBar sprintBar;
+
+    public Image SprintBar;
+
 
     public Vector2 turn;
 
     public TextMeshProUGUI levelUpText;
+
+    public bool running;
+
+    private Coroutine recharge;
+    public float chargeRate;
 
     void Start()
     {
@@ -43,20 +56,31 @@ public class Player : MonoBehaviour
     void Update()
     {
         Movement();
-        //HurtSelf();
-        if (Input.GetKeyDown(KeyCode.Mouse0) && autoFire == false)
+
+        ShootBullet();
+
+        Sprinting();
+
+
+        if (running == true)
         {
-            ShootBullet();
-        }
-        if (Input.GetKeyDown(KeyCode.E) && autoFire == false)
-        {
-            StartCoroutine("Autofire");
-            autoFire = true;
-        }
-        else if (Input.GetKeyDown(KeyCode.E) && autoFire == true)
-        {
-            StopCoroutine("Autofire");
-            autoFire = false;
+            sprint -= sprintCost * Time.deltaTime;
+
+            
+
+            if (sprint == 0)
+            {
+                sprint = 0;
+            }
+
+            sprintBar.SetSprint(sprint);
+
+           if(recharge != null)
+            {
+                StopCoroutine(recharge);
+            }
+           recharge = StartCoroutine(RechargeSprint());
+
         }
     }
     
@@ -69,9 +93,44 @@ public class Player : MonoBehaviour
 
     }
 
-   
+    void Sprinting()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed *= 1.5f;
+
+            running = true;
+            Debug.Log(sprint);
+
+
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift) || sprint <= 0)
+        {
+            speed = 6f;
+            running = false;
+
+        }
+
+
+    }
+
+    private IEnumerator RechargeSprint()
+    {
+        yield return new WaitForSeconds(1f);
+        while (sprint < maxSprint)
+        {
+            sprint += chargeRate / 10f;
+            if (sprint > maxSprint)
+            {
+                sprint = maxSprint;
+            }
+            sprintBar.SetSprint(sprint);
+            yield return new WaitForSeconds(.1f);
+        }
+    }
+
     //does damage to player in 1/3 increments
-     public void Damage(float damageAmount)
+    public void Damage(float damageAmount)
     {
         lives -= damageAmount;
         healthBar.SetHealth(lives);
@@ -118,15 +177,5 @@ public class Player : MonoBehaviour
     void ShootBullet()
     {
         Instantiate(bullet, transform.position, transform.rotation);        
-    }
-
-    IEnumerator Autofire()
-    {
-        while (true)
-        {
-            Instantiate(bullet, transform.position, transform.rotation);
-            Debug.Log("Fired");
-            yield return new WaitForSeconds(0.5f);
-        }
     }
 }
