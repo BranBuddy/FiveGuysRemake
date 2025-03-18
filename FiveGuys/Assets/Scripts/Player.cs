ab3783e31e929768b5742c5642f2219eb9301aa4
BranBuddy <brandonwahl111@gmail.com>
2025-03-17 21:24:40 -0400

    Revert "Merge pull request #11 from BranBuddy/BrandonBranch"
    This reverts commit ddac301fa3099bce8306e2c90e9d153f7df6511c, reversing
    changes made to a96351a83709412656121344b881221de8d4a578.

--- FiveGuys/Assets/Scripts/Player.cs
+++ FiveGuys/Assets/Scripts/Player.cs
@@ -1,4 +1,3 @@
-
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -7,88 +6,43 @@ using UnityEngine.UI;
 using TMPro;
 using UnityEngine.SceneManagement;
 
-
 public class Player : MonoBehaviour
 {
      private float horizontalInput;
      private float verticalInput;
-    public float speed = 6f;
+    public float speed = 5f;
     public float lives = 3f;
     public float maxLives = 3f;
     public float minXP = 0f;
-    public float maxSprint = 1f;
-    public float sprint;
     public float xp;
     public bool autoFire = false;
     public GameObject enemy;
     public GameObject bullet;
     private int charLevel;
-    public float sprintCost;
 
     public Healthbar healthBar;
     public XPBar xpBar;
-    public SprintBar sprintBar;
 
-    public Image SprintBar;
-
-    public bool shouldLerp = false;
-    public float lerpSpeed = 2f;
-    float t = 0;
+    public Vector2 turn;
 
     public TextMeshProUGUI levelUpText;
 
-    public bool running;
-
-    private Coroutine recharge;
-    public float chargeRate;
-
- 
     void Start()
     {
-        sprintCost = 1f;
         charLevel = 1;
         levelUpText.text = "Level: " + charLevel;
         xp = minXP;
         lives = maxLives;
 
         healthBar.SetMaxHealth(maxLives);
-        sprintBar.SetMaxSprint(maxSprint);
         xpBar.SetMinXP(minXP);
-
+        
     }
    
     // Update is called once per frame
     void Update()
     {
         Movement();
-<<<<<<< HEAD
-        ShootBullet();
-        Sprinting();
-
-
-        if (running == true)
-        {
-            sprint -= sprintCost * Time.deltaTime;
-
-            
-
-            if (sprint == 0)
-            {
-                sprint = 0;
-            }
-
-            sprintBar.SetSprint(sprint);
-
-           if(recharge != null)
-            {
-                StopCoroutine(recharge);
-            }
-           recharge = StartCoroutine(RechargeSprint());
-
-        }
-
-
-=======
         //HurtSelf();
         if (Input.GetKeyDown(KeyCode.Mouse0) && autoFire == false)
         {
@@ -104,7 +58,6 @@ public class Player : MonoBehaviour
             StopCoroutine("Autofire");
             autoFire = false;
         }
->>>>>>> main
     }
     
     //moves player
@@ -116,42 +69,7 @@ public class Player : MonoBehaviour
 
     }
 
-   void Sprinting()
-    {
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            speed *= 1.5f;
-
-            running = true;
-            Debug.Log(sprint);
-
-            
-        }
-        if (Input.GetKeyUp(KeyCode.LeftShift) || sprint <= 0)
-        {
-            speed = 6f;
-            running = false;
-
-        }
-
-        
-    }
-
-    private IEnumerator RechargeSprint()
-    {
-        yield return new WaitForSeconds(1f);
-        while (sprint < maxSprint)
-        {
-            sprint += chargeRate / 10f;
-            if (sprint > maxSprint)
-            {
-                sprint = maxSprint;
-            }
-            sprintBar.SetSprint(sprint);
-            yield return new WaitForSeconds(.1f);
-        }
-    }
-    
+   
     //does damage to player in 1/3 increments
      public void Damage(float damageAmount)
     {
@@ -209,15 +127,6 @@ public class Player : MonoBehaviour
             Instantiate(bullet, transform.position, transform.rotation);
             Debug.Log("Fired");
             yield return new WaitForSeconds(0.5f);
-        }
-    }
-
-    public void HealPlayer(int healthGained)
-    {
-        if (lives < maxLives)
-        {
-            lives += healthGained;
-            healthBar.SetHealth(lives);
         }
     }
 }
