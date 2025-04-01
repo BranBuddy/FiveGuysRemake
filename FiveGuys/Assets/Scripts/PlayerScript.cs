5305451ca1fdcaadda3707c356fde864681ad763
BranBuddy <brandonwahl111@gmail.com>
2025-03-31 21:18:27 -0400

    Revert "Merge pull request #17 from BranBuddy/BrandonBranch"
    This reverts commit 69edb868bea896e51133e84c6a0393e6efef0222, reversing
    changes made to 02906bb369b6f12d3fa3dce02cfba07d8bbd04b2.

--- FiveGuys/Assets/Scripts/PlayerScript.cs
+++ FiveGuys/Assets/Scripts/PlayerScript.cs
@@ -6,10 +6,11 @@ using UnityEngine.UI;
 using TMPro;
 using UnityEngine.SceneManagement;
 using System.Security.Cryptography;
+using Cinemachine;
 
 public class PlayerScript : MonoBehaviour
 {
-     private float horizontalInput;
+    private float horizontalInput;
      private float verticalInput;
     public float speed = 5f;
     public float lives = 3f;
@@ -19,18 +20,19 @@ public class PlayerScript : MonoBehaviour
     public float sprint;
     public float xp;
     public bool autoFire;
-    public GameObject enemy;
     public GameObject bulletPrefab;
-    private int charLevel;
+    public int charLevel;
     public float sprintCost;
+
+
+    public AudioClip levelUpClip;
+    public AudioClip bulletClip;
 
     public Healthbar healthBar;
     public XPBar xpBar;
     public SprintBar sprintBar;
 
     public Image stamina;
-
-    public Vector2 turn;
 
     public TextMeshProUGUI levelUpText;
 
@@ -47,9 +49,12 @@ public class PlayerScript : MonoBehaviour
         lives = maxLives;
         autoFire = false;
 
+
         healthBar.SetMaxHealth(maxLives);
         xpBar.SetMinXP(minXP);
         sprintBar.SetSprint(sprint);
+
+
 
     }
    
@@ -150,9 +155,10 @@ public class PlayerScript : MonoBehaviour
 
         if (lives <= 0)
         {
-            
             Destroy(this.gameObject);
+            StartCoroutine(WaitForDeath());
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            
 
         }
     }
@@ -168,6 +174,7 @@ public class PlayerScript : MonoBehaviour
             xpBar.SetXP(xp);
             Debug.Log("lEVEL UP");
             LevelUp();
+            AudioSource.PlayClipAtPoint(levelUpClip, transform.position, .7f);
 
         }
     }
@@ -189,17 +196,26 @@ public class PlayerScript : MonoBehaviour
 
     void ShootbulletPrefab()
     {
-        Instantiate(bulletPrefab, transform.position, transform.rotation);        
+        Instantiate(bulletPrefab, transform.position, transform.rotation); 
+        AudioSource.PlayClipAtPoint(bulletClip, transform.position, .7f);
     }
     IEnumerator Autofire()
     {
         while (true)
         {
             Instantiate(bulletPrefab, transform.position, transform.rotation);
+            AudioSource.PlayClipAtPoint(bulletClip, transform.position, .7f);
             //Debug.Log("Fired");
             yield return new WaitForSeconds(0.5f);
         }
     }
+
+    IEnumerator WaitForDeath()
+    {
+        yield return new WaitForSeconds(3f);
+        
+    }
+
     public void HealPlayer(int healthGained)
     {
         if (lives < maxLives)
@@ -208,4 +224,5 @@ public class PlayerScript : MonoBehaviour
             healthBar.SetHealth(lives);
         }
     }
+
 }
