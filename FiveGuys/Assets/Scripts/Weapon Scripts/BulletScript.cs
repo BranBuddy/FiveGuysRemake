524d78f245d9780ae85eb71c07e5367fdbda5052
Terrarianium <Collinrogers915@gmail.com>
2025-04-17 13:16:17 -0400

    Added rocket, made bullet better.
    Added rocket with explosion that deals damage and fades. Made bullet better.

--- FiveGuys/Assets/Scripts/Weapon Scripts/BulletScript.cs
+++ FiveGuys/Assets/Scripts/Weapon Scripts/BulletScript.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 
 public class BulletScript : MonoBehaviour
 {
+    // Initialization
     public string enemyTag = "Enemy";
     private GameObject[] targetEnemies;
     private Vector3 targetEnemyPosition;
@@ -22,35 +23,37 @@ public class BulletScript : MonoBehaviour
         autoFire = GameObject.FindWithTag("Player").GetComponent<PlayerScript>().autoFire; 
         // finds enemy
         if (autoFire)
-        {
+        { // set enemies in array if auto firing
             targetEnemies = GameObject.FindGameObjectsWithTag(enemyTag);
 
             foreach (GameObject enemy in targetEnemies)
-            {
+            { // get distance to each enemy
                 float distance = Vector3.Distance(transform.position, enemy.transform.position);
 
                 if (distance < smallestDistance)
-                {
+                { // change target enemy to closest
                     smallestDistance = distance;
                     targetEnemyPosition = enemy.transform.position;
                 }
             }
-
+            
+            // get direction
             movement = Vector3.Normalize(targetEnemyPosition - transform.position);
         }
         else
         {
             try
-            {
+            { // get mouse position using raycast
                 Vector3 mouseScreenPosition = Input.mousePosition;
                 Ray ray = Camera.main.ScreenPointToRay(mouseScreenPosition);
                 RaycastHit hit;
 
                 if (Physics.Raycast(ray, out hit))
-                {
+                { // get world position
                     mouseWorldPosition = hit.point + Vector3.up;
                 }
 
+                // get direction
                 movement = Vector3.Normalize(mouseWorldPosition - transform.position);
             }
             catch (System.Exception e)
@@ -60,29 +63,33 @@ public class BulletScript : MonoBehaviour
             }
         }
 
+        // set speed
         movement = movement * Time.deltaTime * bulletSpeed;
 
+        // delete bullet if not hitting
         Invoke("DeleteSelf", 3f);
     }
 
     void LateUpdate()
-    {
+    { // move bullet
         rb.MovePosition(transform.position + movement);
     }
 
     void DeleteSelf()
-    {
+    { // delete bullet
         Destroy(this.gameObject);
     }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == enemyTag)
-        { // destroy both enemy and bullet
-            other.GetComponent<EnemyFollow>().enemyDamaged(1);
+        { // damage enemy if hitting enemy
+            other.GetComponent<EnemyFollow>().enemyDamaged(1);            
+        }
 
-            DeleteSelf();
-            
+        if (other.tag != "Player")
+        { // destory self when hitting something that's not player
+            Destroy(other.gameObject);
         }
     }
 }
