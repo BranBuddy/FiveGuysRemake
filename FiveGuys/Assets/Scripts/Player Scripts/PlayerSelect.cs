5f9cfd6b1df5ddcd07b30f0424ca9ed57e3d181e
BranBuddy <brandonwahl111@gmail.com>
2025-04-19 19:25:37 -0400

    help

--- FiveGuys/Assets/Scripts/Player Scripts/PlayerSelect.cs
+++ FiveGuys/Assets/Scripts/Player Scripts/PlayerSelect.cs
@@ -21,19 +21,19 @@ public class PlayerSelect : MonoBehaviour
     public void SelectPlayerOne()
     {
         GameObject.Find("Player(Clone)").GetComponent<PlayerScript>().maxLives = 3;
-        GameObject.Find("Player(Clone)").GetComponent<PlayerScript>().baseSpeed = 7;
+        GameObject.Find("Player(Clone)").GetComponent<PlayerScript>().speed = 7;
     }
 
     public void SelectPlayerTwo()
     {
         GameObject.Find("Player(Clone)").GetComponent<PlayerScript>().maxLives = 1;
-        GameObject.Find("Player(Clone)").GetComponent<PlayerScript>().baseSpeed = 10;
+        GameObject.Find("Player(Clone)").GetComponent<PlayerScript>().speed = 10;
 
     }
 
     public void SelectPlayerThree()
     {
         GameObject.Find("Player(Clone)").GetComponent<PlayerScript>().maxLives = 5;
-        GameObject.Find("Player(Clone)").GetComponent<PlayerScript>().baseSpeed = 5;
+        GameObject.Find("Player(Clone)").GetComponent<PlayerScript>().speed = 5;
     }
 }
